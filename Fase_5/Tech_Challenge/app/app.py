616fc90759c5576b028c056033bba0e915e3d1f6
gteless <gabrieltelees@gmail.com>
2026-02-11 20:34:28 -0300

    atualizar datathon

--- Fase_5/Tech_Challenge/app/app.py
+++ Fase_5/Tech_Challenge/app/app.py
@@ -4,10 +4,10 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 # carregar dados
-df = pd.read_csv(r"C:\Users\gabri\Desktop\FIAP\Fase_5\Tech_Challenge\data\base_final.csv")  # salve seu df como csv antes
+#df = pd.read_csv(r"C:\Users\gabri\Desktop\FIAP\Fase_5\Tech_Challenge\data\base_final.csv")  # salve seu df como csv antes
+df = pd.read_parquet(r"C:\Users\gabri\Desktop\FIAP\Fase_5\Tech_Challenge\data\base.parquet")
 cols = ["IAN","IDA","IEG","IAA","IPS","IPP","IPV","INDE"]
 df[cols] = df[cols].apply(pd.to_numeric, errors="coerce")
-
 
 st.set_page_config(layout="wide")
 
@@ -29,6 +29,7 @@ st.header("Visão Geral do Programa")
 media = df.groupby("ANO")[["IAN","IDA","IEG","IAA","IPS","IPP","IPV","INDE"]].mean().reset_index()
 
 fig = px.line(media, x="ANO", y="INDE", markers=True, title="Evolução média do INDE")
+fig.update_xaxes(type="category")
 st.plotly_chart(fig, use_container_width=True)
 
 # ----------------------------
@@ -84,4 +85,6 @@ st.plotly_chart(fig_corr, use_container_width=True)
 st.header("Evolução do aluno")
 
 fig_ind = px.line(df_aluno, x="ANO", y="INDE", markers=True)
+fig_ind.update_xaxes(type="category")
 st.plotly_chart(fig_ind, use_container_width=True)
+
